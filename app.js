c2ae6605c84267cc4f9c0ccc54cc3e5f72e2193d
johnairflare <32536544+johnairflare@users.noreply.github.com>
2017-10-31 17:30:03 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.get('/tweets/:lat/:long', require('./controllers/twitterController'));
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-var port = 8080;
-app.listen(port, () => {
-  console.log('hada api running '+port+'!');
+
+app.get('/', function (req, res) { res.send('Welcome!');});
+app.get('/tweets/:lat/:long', require('./controllers/twitterController'));
+
+app.listen(app.get('port'), app.get('ip'), function () {
+
+    console.log( "Hada api on " + app.get('ip') + ", server_port " + app.get('port')  );
+
 });

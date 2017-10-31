9f1840eae7de55e20c4a66d215db2e60dccbf0a7
johnairflare <32536544+johnairflare@users.noreply.github.com>
2017-10-31 17:35:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const app = express();
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
+app.get('/', function (req, res) { res.send('Welcome!');});
+app.get('/tweets/:lat/:long', require('./controllers/twitterController'));
+
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
-app.get('/', function (req, res) { res.send('Welcome!');});
-app.get('/tweets/:lat/:long', require('./controllers/twitterController'));
 
 app.listen(app.get('port'), app.get('ip'), function () {
 
